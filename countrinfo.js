e9315279ca251792ab38842dac3283d4bd80d01b
Felipe de Senco <55833207+Dijoig@users.noreply.github.com>
2020-11-10 10:52:14 +0000

    Add files via upload
    initial setup

---
+++ countrinfo.js
@@ -0,0 +1,5 @@
+var worldMap = L.map('mapId').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+}).addTo(worldMap);
