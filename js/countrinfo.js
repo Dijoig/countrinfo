762c921587362c49b5cc6495eeee42db6e8929f0
Felipe de Senco <55833207+Dijoig@users.noreply.github.com>
2020-11-14 10:34:13 +0000

    borders succesful
    Added the logic to countryBordershandler to get the geojson feature of the country selected by the user. Updated the logic of the secces function of the ajax request to display the country and highlight it, using map.fitbounds to center it.
    
    added a clear map button, still undecided if will be used.

--- js/countrinfo.js
+++ js/countrinfo.js
@@ -1,16 +1,16 @@
 
 //map setup:
-var worldMap = L.map('mapId').setView([51.505, -0.09], 13);
+var worldMap = L.map('mapId').setView([51.505, -0.09], 6);
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 }).addTo(worldMap);
 
-//request to countryBorderHandler to get borders:
+
+//request to php  routine that retrieves the geojson feature for the selected country at the nav bar:
 var $goBtn = $("#GoBtn");
 var $countryCode = $('#CountrySelection');
-
-
+var feature;
 $goBtn.click(function() {
   $.ajax({
     url: './php/countryBordersHandler.php',
@@ -18,10 +18,20 @@ $goBtn.click(function() {
     dataType: 'json',
     data: {countryCode: $countryCode.val()},
     success: function(result) {
-      console.log(result);
+      if (feature) {
+        feature.clearLayers();
+      }
+      console.log(result.geometry.coordinates);
+      var myStyle = {"color": "#2D5EF9", "weight": 4, "opacity": 0.5};
+      feature = L.geoJSON(result, {style: myStyle}).addTo(worldMap);
+      worldMap.fitBounds(feature.getBounds());
     },
     error: function(error) {
       console.log(error);
     }
   });
 });
+
+//logic for the clear map button:
+
+
--- php/countryBordersHandler.php
+++ php/countryBordersHandler.php
@@ -1,12 +1,26 @@
 <?php
+
+//storing the properties countryCode from the global request variable from the ajax request:
+$countryCode = $_REQUEST['countryCode'];
 //getting contents from countryBorders.geo.json decoding and storing in a variable:
+$encodedBorder = file_get_contents('../json/countryBorders.geo.json');
 
+$border = json_decode($encodedBorder, true);
 
-$myJSON = json_encode($_REQUEST["countryCode"]);
+$features = $border['features'];
 
+//logic to match the iso chosen from the nav bar to the right contry in countryBorders.geo.json:
+foreach($features as $feature) {
+  $iso3 = $feature['properties']['iso_a3'];
+  if ($countryCode == $iso3) {
+    $countryGeoJson = $feature;
+    //outputing the right coordinates:
+    $output = json_encode($countryGeoJson);
+    echo $output;
+  }
+  
+  
+}
 
-
-echo $myJSON;
-//storing the iso code from the select element at html in a variable:
 
 ?>
