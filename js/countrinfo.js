b38d2589b2a99eb5a3a513fc825353cc10e1ac53
Felipe de Senco <55833207+Dijoig@users.noreply.github.com>
2020-11-18 11:53:44 +0000

    User location added with functionality
    Added the user location functionality to load the page highlighting the country the user is in. As well as condensed the 2 calls to Countrybordershandler into one function that can be used repeatedly: ajaxCountryBorder.

--- js/countrinfo.js
+++ js/countrinfo.js
@@ -1,3 +1,61 @@
+//defining the function that will make the ajax request to the php routine handling the country borders:
+const ajaxCountryBorders = function(iso3) {
+  $.ajax({
+           url: './php/countryBordersHandler.php',
+          type: 'POST',
+          dataType: 'json',
+          data: {countryCode: iso3},
+          success: function(result) {
+            if (feature) {
+              feature.clearLayers();
+            }
+            console.log(result.geometry.coordinates);
+            var myStyle = {"color": "#2D5EF9", "weight": 4, "opacity": 0.5};
+            feature = L.geoJSON(result, {style: myStyle}).addTo(worldMap);
+            worldMap.fitBounds(feature.getBounds());
+          },
+          error: function(error) {
+            console.log(error);
+          }
+        });
+      }
+//declaring onready code:
+$('document').ready(function() {
+  //getting user coords:
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      var userLat = pos.coords.latitude;
+      var userLng = pos.coords.longitude;
+      
+  //making call to opencage reverse geocoding api with the coords:
+      $.ajax({
+        url: './php/openCageReverse.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          lat: userLat,
+          lng: userLng
+        },
+        success: function(result) {
+          var userISO3 = result['data'][0]['components']["ISO_3166-1_alpha-3"];
+  //call to the ajax request to handle borders using the user iso3 code:
+          ajaxCountryBorders(userISO3);
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
+      
+    }, function(error) {
+      console.log(error);
+    })
+  } else {
+    alert("Your browser doesn't support geolocation, functions that require your location won't be avaiable... You can update your browser or use another one.");
+  }
+  
+});
+//getting user iso3 using opencage reverse geocoding:
+
 
 //map setup:
 var worldMap = L.map('mapId').setView([51.505, -0.09], 6);
@@ -7,29 +65,14 @@ L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map
 }).addTo(worldMap);
 
 
+
 //request to php  routine that retrieves the geojson feature for the selected country at the nav bar:
 var $goBtn = $("#GoBtn");
 var $countryCode = $('#CountrySelection');
 var feature;
+//Go button event listener to make a call to the border handler function, using the selected iso3 from the nav bar:
 $goBtn.click(function() {
-  $.ajax({
-    url: './php/countryBordersHandler.php',
-    type: 'POST',
-    dataType: 'json',
-    data: {countryCode: $countryCode.val()},
-    success: function(result) {
-      if (feature) {
-        feature.clearLayers();
-      }
-      console.log(result.geometry.coordinates);
-      var myStyle = {"color": "#2D5EF9", "weight": 4, "opacity": 0.5};
-      feature = L.geoJSON(result, {style: myStyle}).addTo(worldMap);
-      worldMap.fitBounds(feature.getBounds());
-    },
-    error: function(error) {
-      console.log(error);
-    }
-  });
+  ajaxCountryBorders($('#CountrySelection').val());
 });
 
 //logic for the clear map button:
